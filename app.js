51545af172d13b7c54471e30fc0dd688042fe090
Aziz.AZIZI <mohamedaziz.azizi@esprit.tn>
2024-03-06 10:03:49 +0100

    azizi_commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const InstitutionRoutes = require('./routes/InstitutionRoutes');
+const CategoryRoutes = require('./routes/CategoryRoutes');
+const SubcategoryRoutes = require('./routes/SubcategoryRoutes');
+const connectDB = require('./config/database');
+const app = express();
+
+app.use(express.json());
+// Activer CORS
+app.use(cors({
+  origin: 'http://localhost:3001'
+}));
+
+// Connect to MongoDB database
+connectDB().then(() => {
+  // Use Institution routes after establishing connection to the database
+  app.use('/', InstitutionRoutes);
+  
+  // Use Category routes after establishing connection to the database
+  app.use('/', CategoryRoutes);
+
+  // Use Subcategory routes after establishing connection to the database
+  app.use('/', SubcategoryRoutes);
+
+
+
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
+}).catch((err) => {
+  console.error("Error connecting to MongoDB :", err);
+});
