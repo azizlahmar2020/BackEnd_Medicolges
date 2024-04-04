ffd47499ae1b1f836400c9f8c6106f91f82d2f9e
SarraSahli1 <126513642+SarraSahli1@users.noreply.github.com>
2024-04-04 01:23:20 +0100

    commitBack

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -5,7 +5,6 @@ const userController = require('../controllers/UserController');
 router.get('/showUsers', userController.showUsers);
 router.get('/getUser/:id', userController.getUserById);
 
-router.put('/updateUser/:id', userController.updateUser);
 router.delete('/deleteUser/:id', userController.deleteUser);
 router.put('/updateUserRole/:id', userController.updateUserRole);
 router.get('/getUserByEmail/:email', userController.getUserByEmail); // New route for getUserByEmail
